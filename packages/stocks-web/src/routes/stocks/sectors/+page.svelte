2851c2044b4980e1664d86a50471b121215b0ffd
Mauricio Piber <mauriciopiber@gmail.com>
2022-09-09 04:42:28 -0300

    Added icons

--- packages/stocks-web/src/routes/stocks/sectors/+page.svelte
+++ packages/stocks-web/src/routes/stocks/sectors/+page.svelte
@@ -1,10 +1,58 @@
 <script lang="ts">
-  import { Breadcrumb, BreadcrumbItem } from 'carbon-components-svelte';
-  import Income from '@/components/Stocks/Income.svelte';
-
+  import {
+    Breadcrumb,
+    BreadcrumbItem,
+    DataTable,
+    Pagination,
+    Toolbar,
+    ToolbarContent,
+    ToolbarSearch,
+    Link
+  } from 'carbon-components-svelte';
+  import Rate from '@/components/Layout/Rate.svelte';
+  import Currency from '@/components/Layout/Currency.svelte';
+  import CenterCircle from 'carbon-icons-svelte/lib/CenterCircle.svelte';
   import type { SectorQuery } from '@pibernetwork/stocks-model/src/types';
   export let data: { sectors: SectorQuery[] };
   const { sectors } = data;
+
+  let pageSize = 25;
+  let page = 1;
+  let filteredRowIds: string[] = [];
+  const headers = [
+    {
+      key: 'slug',
+      value: ''
+    },
+    {
+      key: 'name',
+      value: 'Name'
+    },
+    {
+      key: 'subSectors',
+      value: '# SubSector'
+    },
+
+    {
+      key: 'averageAmount',
+      value: 'Average Amount'
+    },
+    {
+      key: 'averageYield',
+      value: 'Average Yield'
+    }
+  ];
+
+  const rows = sectors.map((sector) => {
+    return {
+      id: sector.slug,
+      name: sector.name,
+      slug: sector.slug,
+      subSectors: sector.subSectors.length,
+      averageAmount: sector.income.averageAmount,
+      averageYield: sector.income.averageYield
+    };
+  });
 </script>
 
 <Breadcrumb noTrailingSlash>
@@ -14,9 +62,22 @@
 </Breadcrumb>
 
 <h1>Sectors</h1>
-{#each sectors as sector}
-  <div>
-    <a href="/stocks/sectors/{sector.slug}">{sector.name}</a>
-    <Income averageIncome={sector.income.averageAmount} averageYield={sector.income.averageYield} />
-  </div>
-{/each}
+<DataTable sortable size="short" {headers} {rows} {pageSize} {page}>
+  <svelte:fragment slot="cell" let:cell>
+    {#if cell.key === 'averageAmount'}
+      <Currency value={cell.value} />
+    {:else if cell.key === 'averageYield'}
+      <Rate value={cell.value} />
+    {:else if cell.key === 'slug'}
+      <Link href={`/stocks/sectors/${cell.value}`}><CenterCircle size={20} /></Link>
+    {:else}
+      {cell.value}
+    {/if}
+  </svelte:fragment>
+  <Toolbar>
+    <ToolbarContent>
+      <ToolbarSearch persistent value="" shouldFilterRows bind:filteredRowIds />
+    </ToolbarContent>
+  </Toolbar>
+</DataTable>
+<Pagination bind:pageSize bind:page totalItems={filteredRowIds.length} pageSizeInputDisabled />
