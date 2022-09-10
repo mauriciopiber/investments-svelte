da00d562773463694408a9e43b30fd1a1b07b9ca
Mauricio Piber <mauriciopiber@gmail.com>
2022-09-10 00:25:55 -0300

    Change build folder to public

--- packages/stocks-server/bin/build.mjs
+++ packages/stocks-server/bin/build.mjs
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import esbuild from "esbuild";
 
-const buildDirectory = "dist";
+const buildDirectory = "public";
 const production = process.env.NODE_ENV === "production";
 
 // Config entrypoint files
